5a2d13fbf3870b8cb937875418bd9763fdea2327
Itscientgourav <gouravvish.itscient@gmail.com>
2023-11-06 07:58:58 +0530

    Navbar Design

--- smls/resources/views/landing/home.blade.php
+++ smls/resources/views/landing/home.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>School Management System</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="https://www.paatham.in/assets/images/favicon.png">
     <link rel="stylesheet" href="{{ asset('Css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('Css/fontawesome.min.css') }}">
     <link rel="stylesheet" href="{{ asset('Css/navbar/navbar.css') }}">
@@ -14,7 +15,7 @@
 <body>
     <nav>
         <div class="wrapper">
-            <div class="logo"><a href="#">Logo</a></div>
+            <div class="logo"><a href="#"><img src="{{ asset('uploads/Logo/learn_logo.png') }}" alt="Logo" width="90"></a></div>
             <input type="radio" name="slider" id="menu-btn">
             <input type="radio" name="slider" id="close-btn">
             <ul class="nav-links" style="margin-bottom: 0px !important;">
@@ -77,10 +78,48 @@
         </div>
     </nav>
 
-    <div class="body-text">
-        <div class="title">Responsive Dropdown and Mega Menu</div>
-        <div class="sub-title">using only HTML & CSS</div>
-    </div>
+    <div class="container-fluid">
+   <div class="card">
+    <div id="carouselExampleDark" class="carousel slide" data-ride="carousel" data-interval="1000">
+        <div class="carousel-indicators">
+          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        </div>
+        <div class="carousel-inner">
+          <div class="carousel-item active" data-bs-interval="10000">
+            <img src="{{ asset('uploads/carousel/first.png') }}" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>Some representative placeholder content for the first slide.</p>
+            </div>
+          </div>
+          <div class="carousel-item" data-bs-interval="2000">
+            <img src="{{ asset('uploads/carousel/second.png') }}" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>Some representative placeholder content for the second slide.</p>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img src="{{ asset('uploads/carousel/third.png') }}" class="d-block w-100" alt="...">
+            <div class="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>Some representative placeholder content for the third slide.</p>
+            </div>
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+   </div>
+</div>
 </body>
 <script src="{{ asset('Js/bootstrap.bundle.min.js') }}"></script>
 </html>
