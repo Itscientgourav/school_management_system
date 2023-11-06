49735722515096deb070adfbece2f949984a31f0
Itscientgourav <gouravvish.itscient@gmail.com>
2023-11-06 11:27:38 +0530

    carousel remove

--- smls/resources/views/landing/home.blade.php
+++ smls/resources/views/landing/home.blade.php
@@ -2,92 +2,81 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>School Management System</title>
-    <link rel="icon" type="image/png" sizes="16x16" href="https://www.paatham.in/assets/images/favicon.png">
-    <link rel="stylesheet" href="{{ asset('Css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('Css/fontawesome.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('Css/navbar/navbar.css') }}">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>School Management System</title>
+  <link rel="icon" type="image/png" sizes="18x18" href="{{ asset('uploads/Logo/learn_logo.png') }}">
+  <link rel="stylesheet" href="{{ asset('Css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('Css/fontawesome.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('Css/navbar/navbar.css') }}">
 </head>
 
 <body>
-    <nav>
-        <div class="wrapper">
-            <div class="logo"><a href="#"><img src="{{ asset('uploads/Logo/learn_logo.png') }}" alt="Logo" width="90"></a></div>
-            <input type="radio" name="slider" id="menu-btn">
-            <input type="radio" name="slider" id="close-btn">
-            <ul class="nav-links" style="margin-bottom: 0px !important;">
-                <label for="close-btn" class="btn close-btn"><i class="fas fa-times"></i></label>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About</a></li>
-                <!-- <li>
-                    <a href="#" class="desktop-item">Dropdown Menu</a>
-                    <input type="checkbox" id="showDrop">
-                    <label for="showDrop" class="mobile-item">Dropdown Menu</label>
-                    <ul class="drop-menu">
-                        <li><a href="#">Drop menu 1</a></li>
-                        <li><a href="#">Drop menu 2</a></li>
-                        <li><a href="#">Drop menu 3</a></li>
-                        <li><a href="#">Drop menu 4</a></li>
-                    </ul>
-                </li> -->
-                <li>
-                    <a href="#" class="desktop-item">Mega Menu</a>
-                    <input type="checkbox" id="showMega">
-                    <label for="showMega" class="mobile-item">Mega Menu</label>
-                    <div class="mega-box">
-                        <div class="content">
-                            <div class="row">
-                                <img src="https://fadzrinmadu.github.io/hosted-assets/responsive-mega-menu-and-dropdown-menu-using-only-html-and-css/img.jpg" alt="">
-                            </div>
-                            <div class="row">
-                                <header>Design Services</header>
-                                <ul class="mega-links">
-                                    <li><a href="#">Graphics</a></li>
-                                    <li><a href="#">Vectors</a></li>
-                                    <li><a href="#">Business cards</a></li>
-                                    <li><a href="#">Custom logo</a></li>
-                                </ul>
-                            </div>
-                            <div class="row">
-                                <header>Email Services</header>
-                                <ul class="mega-links">
-                                    <li><a href="#">Personal Email</a></li>
-                                    <li><a href="#">Business Email</a></li>
-                                    <li><a href="#">Mobile Email</a></li>
-                                    <li><a href="#">Web Marketing</a></li>
-                                </ul>
-                            </div>
-                            <div class="row">
-                                <header>Security services</header>
-                                <ul class="mega-links">
-                                    <li><a href="#">Site Seal</a></li>
-                                    <li><a href="#">VPS Hosting</a></li>
-                                    <li><a href="#">Privacy Seal</a></li>
-                                    <li><a href="#">Website design</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-                <li><a href="#">Feedback</a></li>
-            </ul>
-            <label for="menu-btn" class="btn menu-btn"><i class="fas fa-bars"></i></label>
-        </div>
-    </nav>
+  <nav>
+    <div class="wrapper">
+      <div class="logo"><a href="#"><img src="{{ asset('uploads/Logo/learn_logo.png') }}" alt="Logo" width="90"></a></div>
+      <input type="radio" name="slider" id="menu-btn">
+      <input type="radio" name="slider" id="close-btn">
+      <ul class="nav-links" style="margin-bottom: 0px !important;">
+        <label for="close-btn" class="btn close-btn"><i class="fas fa-times"></i></label>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li>
+          <a href="#" class="desktop-item">Mega Menu</a>
+          <input type="checkbox" id="showMega">
+          <label for="showMega" class="mobile-item">Mega Menu</label>
+          <div class="mega-box">
+            <div class="content">
+              <div class="row">
+                <img src="https://fadzrinmadu.github.io/hosted-assets/responsive-mega-menu-and-dropdown-menu-using-only-html-and-css/img.jpg" alt="">
+              </div>
+              <div class="row">
+                <header>Design Services</header>
+                <ul class="mega-links">
+                  <li><a href="#">Graphics</a></li>
+                  <li><a href="#">Vectors</a></li>
+                  <li><a href="#">Business cards</a></li>
+                  <li><a href="#">Custom logo</a></li>
+                </ul>
+              </div>
+              <div class="row">
+                <header>Email Services</header>
+                <ul class="mega-links">
+                  <li><a href="#">Personal Email</a></li>
+                  <li><a href="#">Business Email</a></li>
+                  <li><a href="#">Mobile Email</a></li>
+                  <li><a href="#">Web Marketing</a></li>
+                </ul>
+              </div>
+              <div class="row">
+                <header>Security services</header>
+                <ul class="mega-links">
+                  <li><a href="#">Site Seal</a></li>
+                  <li><a href="#">VPS Hosting</a></li>
+                  <li><a href="#">Privacy Seal</a></li>
+                  <li><a href="#">Website design</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </li>
+        <li><a href="#">Feedback</a></li>
+      </ul>
+      <label for="menu-btn" class="btn menu-btn"><i class="fas fa-bars"></i></label>
+    </div>
+  </nav>
 
-    <div class="container-fluid">
-   <div class="card">
-    <div id="carouselExampleDark" class="carousel slide" data-ride="carousel" data-interval="1000">
+  <!-- <div class="container-fluid">
+    <div class="card">
+      <div id="carouselExampleDark" class="carousel slide" data-ride="carousel" data-interval="1000">
         <div class="carousel-indicators">
           <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
           <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
           <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
         <div class="carousel-inner">
-          <div class="carousel-item active" data-bs-interval="10000">
+          <div class="carousel-item active" data-bs-interval="2000">
             <img src="{{ asset('uploads/carousel/first.png') }}" class="d-block w-100" alt="...">
             <div class="carousel-caption d-none d-md-block">
               <h5>First slide label</h5>
@@ -118,8 +107,14 @@
           <span class="visually-hidden">Next</span>
         </button>
       </div>
-   </div>
-</div>
+    </div>
+  </div> -->
+
+  <div class="body-text">
+        <div class="title">Responsive Dropdown and Mega Menu</div>
+        <div class="sub-title">using only HTML & CSS</div>
+    </div>
 </body>
 <script src="{{ asset('Js/bootstrap.bundle.min.js') }}"></script>
+
 </html>
